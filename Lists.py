1725304e19a960c917d75a241ba44820b3a03ee8
Lukas Müntinga <lukas.muentinga@icloud.com>
2021-04-22 16:18:27 +0200

    Changed to object oriented usage

--- Lists.py
+++ Lists.py
@@ -1,61 +1,65 @@
 """ Methods for list manipulation based on https://developer.atlassian.com/cloud/trello/rest/api-group-lists/#api-lists-id-get"""
 
 
-from . import Trello
+from .Trello import Trello
+from .Boards import Board
+from typing import Union
+
+
+class List:
+
+    __id: str
+    __trello: Trello
+
+    def __init__(self, trello: Trello, id: str):
+        """ List object for lists in Trello boards.
+
+        Args:
+            trello (Trello): Session to trello
+            id (str): Id of the list
+        """
+
+    def update():
+        # TODO: implement
+        pass
+
+    def archive():
+        # TODO: implement
+        pass
+
+    def move_to_board(board: Union[Board, str]):
+        # TODO: implement
+        pass
+
+    def unarchive():
+        # TODO: implement
+        pass
+
+    def archive_all_cards():
+        # TODO: implement
+        pass
+
+    def move_all_cards():
+        # TODO: implement
+        pass
+
+    def update_field():
+        # TODO: implement
+        pass
+
+    def get_actions():
+        # TODO: implement
+        pass
+
+    def get_board():
+        # TODO: implement
+        pass
+
+    def get_cards():
+        # TODO: implement
+        pass
 
 
 def get_list():
     # TODO: implement
     pass
-
-
-def update_list():
-    # TODO: implement
-    pass
-
-
-def archive_list():
-    # TODO: implement
-    pass
-
-
-def move_list_to_board():
-    # TODO: implement
-    pass
-
-
-def unarchive_list():
-    # TODO: implement
-    pass
-
-
-def archive_all_cards():
-    # TODO: implement
-    pass
-
-
-def move_all_cards():
-    # TODO: implement
-    pass
-
-
-def update_list_field():
-    # TODO: implement
-    pass
-
-
-def get_list_actions():
-    # TODO: implement
-    pass
-
-
-def get_list_board():
-    # TODO: implement
-    pass
-
-
-def get_list_cards():
-    # TODO: implement
-    pass
-
-
