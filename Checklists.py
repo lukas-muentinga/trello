b420e10934ab5b094e96b4b4914ed99698b001c0
Lukas Müntinga <lukas.muentinga@icloud.com>
2021-05-06 00:18:19 +0200

    Implemented create_checkitem()

--- Checklists.py
+++ Checklists.py
@@ -1,4 +1,5 @@
 from .Trello import Trello
+from typing import Union
 
 
 class Checklist():
@@ -37,9 +38,24 @@ class Checklist():
         # TODO:implement
         pass
 
-    def create_checkitem(self):
-        # TODO:implement
-        pass
+    def create_checkitem(self,
+                         name: str,
+                         position: Union(str, int) = 'bottom',
+                         is_checked: bool = False):
+        """Creates a checklist item on the checklist
+
+        Args:
+            name (str): Title of the checklist item
+            position (str or int, optional): Position in the checklist. Either 'top', 'bottom' or int.
+                Defaults to 'bottom'.
+            is_checked (bool, optional): Whether the checklist item should be checked. Defaults to False.
+        """
+        data = {
+            'name': name,
+            'pos': position,
+            'checked': is_checked,
+        }
+        self.__trello.post(f'checklists/{self.__id}/checkItem', json=data)
 
     def delete_checkitem(self):
         # TODO:implement
