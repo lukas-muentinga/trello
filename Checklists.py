ab8909eb363d70f30129810d2e693fdd7ed74c35
Lukas Müntinga <lukas.muentinga@icloud.com>
2021-05-06 00:13:16 +0200

    Added method bodies

--- Checklists.py
+++ Checklists.py
@@ -1,3 +1,48 @@
-from . import Trello
+from .Trello import Trello
 
-# TODO: write method bodies
+
+class Checklist():
+    __id: str
+    __trello: Trello
+
+    def __init__(self, trello: Trello, definition: dict):
+        self.__id = definition['id']
+        self.__trello = trello
+
+    def update(self):
+        # TODO:implement
+        pass
+
+    def delete(self):
+        # TODO:implement
+        pass
+
+    def get_field(self):
+        # TODO:implement
+        pass
+
+    def update_field(self):
+        # TODO:implement
+        pass
+
+    def get_board(self):
+        # TODO:implement
+        pass
+
+    def get_card(self):
+        # TODO:implement
+        pass
+
+    def get_checkitem(self):
+        # TODO:implement
+        pass
+
+    def create_checkitem(self):
+        # TODO:implement
+        pass
+
+    def delete_checkitem(self):
+        # TODO:implement
+        pass
+
+    
